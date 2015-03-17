e37f86042e7d116fc319ddba2c5f2864e0a0710f
liming <liming712@qq.com>
2015-03-17 22:25:28 +0800

    create HDF5 in finetune_fcn is ok for MSRA Data

--- examples/finetune_fcn/hdf5_data/create_caffe_data.py
+++ examples/finetune_fcn/hdf5_data/create_caffe_data.py
@@ -4,14 +4,14 @@ import os
 import h5py
 import numpy as np
 import cv2
-def process_image(filelist,M,N):
+def process_image(imageDir,filelist,M,N):
     with open(filelist) as f:
         content1 = f.readlines()
     content=content1#[0:10]
     imageData=np.zeros([len(content),3,M,N]);
     idx=0
     for string in content:
-        filename='/home/liming/project/dataset/JPEGImages/'+string[0:10]
+        filename=imageDir+string[0:-1]
         transformer = caffe.io.Transformer({'data': (1,3,M,N)})
         transformer.set_transpose('data', (2,0,1))
         transformer.set_mean('data', np.array([104.00698793,  116.66876762,  122.67891434])) # mean pixel
@@ -22,14 +22,14 @@ def process_image(filelist,M,N):
         idx=idx+1
     return imageData
     
-def process_map(filelist,M,N):
+def process_map(imageDir,filelist,M,N):
     with open(filelist) as f:
         content2 = f.readlines()
     content=content2#[0:10]
     inputMap=np.zeros([len(content),1,M,N]);
     idx=0
     for string in content:
-        filename='/home/liming/project/dataset/label/'+string[0:10]
+        filename=imageDir+string[0:-1]
         image=cv2.imread(filename,0)
         image=cv2.resize(image,(M,N))
         image[image>0]=1
@@ -40,15 +40,22 @@ def process_map(filelist,M,N):
 
 M=500
 N=500
-trainData=process_image('/home/liming/project/dataset/script/traindata.txt' ,M,N)
-trainMap=process_map('/home/liming/project/dataset/script/trainlabel.txt',M,N)
+#file_dir='/home/liming/project/dataset/VOC/'
+#trainData=process_image(file_dir+'JPEGImages/',file_dir+'script/traindata.txt' ,M,N)
+#trainMap=process_map(file_dir+'JPEGImages/',file_dir+'script/trainlabel.txt',M,N)
+#
+#testData=process_image(file_dir+'JPEGImages/',file_dir+'script/testdata.txt' ,M,N)
+#testMap=process_map(file_dir+'JPEGImages/',file_dir+'script/testlabel.txt',M,N)
+file_dir='/home/liming/project/dataset/MSRA/MSRA10K_Imgs_GT/'
+trainData=process_image(file_dir,file_dir+'trainImage.txt' ,M,N)
+trainMap=process_map(file_dir,file_dir+'trainMap.txt',M,N)
 
-testData=process_image('/home/liming/project/dataset/script/testdata.txt' ,M,N)
-testMap=process_map('/home/liming/project/dataset/script/testlabel.txt',M,N)
+testData=process_image(file_dir,file_dir+'testImage.txt' ,M,N)
+testMap=process_map(file_dir,file_dir+'testMap.txt',M,N)
 
-with h5py.File(os.path.dirname(__file__) + '/train.h5', 'w') as f:
+with h5py.File(os.path.dirname(__file__) + '/train_MSRA.h5', 'w') as f:
     f['data'] = trainData
     f['inputmap'] = trainMap
-with h5py.File(os.path.dirname(__file__) + '/test.h5', 'w') as f:
+with h5py.File(os.path.dirname(__file__) + '/test_MSRA.h5', 'w') as f:
     f['data'] = testData
     f['inputmap'] = testMap
